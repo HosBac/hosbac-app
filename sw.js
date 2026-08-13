e685ae818d0d287fabde457c2a0194312d7d24a2
HosBac <support.hosbac@gmail.com>
2026-08-13 12:00:57 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+// sw.js – Service Worker minimal pour HosBac
+const CACHE_NAME = 'hosbac-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/favicon.png',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/apple-touch-icon.png'
+];
+
+// Installation : mise en cache des ressources statiques
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activation : nettoyage des anciens caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interception des requêtes : stratégie "Cache First"
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
