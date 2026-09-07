9bd5eb95bb99e0213c5adc0ee635bfd0784f36b8
HosBac <support.hosbac@gmail.com>
2026-09-07 19:23:55 +0100

    Fix Vercel: Renommage en _db.js pour eviter la compilation serverless de l'utilitaire

--- api/user-profile.js
+++ api/user-profile.js
@@ -1,4 +1,4 @@
-import { executeQuery } from './db.js';
+import { executeQuery } from './_db.js';
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
