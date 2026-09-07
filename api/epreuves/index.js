318761ff9c6db91cc72895ac705b386b24a0c446
HosBac <support.hosbac@gmail.com>
2026-09-07 12:43:14 +0100

    Fix: vercel.json moderne et routes API epreuves

--- api/epreuves/index.js
+++ api/epreuves/index.js
@@ -1,9 +1,9 @@
 import { db } from '../../lib/db.js';
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN || process.env.ALLOWED_ORIGIN || '*');
-  res.setHeader('Vary', 'Origin');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-User-UID, X-UID, X-Firebase-UID');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
 
